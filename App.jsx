bd2e5ad8a213421ba63e30d258473f864344858a
Khushitiwari9870 <150013430+Khushitiwari9870@users.noreply.github.com>
2025-08-01 23:03:23 +0530

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,29 @@
+// import LoginRegister from "./Component/LoginRegister";
+// import Form from "./Component/pages/Form";
+// function App() {
+//   return(
+//     <>
+//     <div className="">
+//       {/* <LoginRegister /> */}
+    
+//       <div className="md:flex flex-1">
+//         <Form />
+//         {/* <Card /> */}
+//       </div>
+//       </div>
+//     </>
+//   );
+// }
+// export default App;
+ 
+import LoginRegister from "./Component/LoginRegister";
+
+function App() {
+  return (
+    <div>
+      <LoginRegister />
+    </div>
+  );
+}
+export default App;
+
